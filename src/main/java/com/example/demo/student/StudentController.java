bc44dd671805daf5cc3416190768df137cfbaa7a
ClydeRM <clyde.official@icloud.com>
2023-03-20 21:42:09 +0800

    feat(Controller): Autowired (AutoInject dependency)
    @Autowired
    public class Some{
    	private final Dep
    dep;
    	@Autowired()
    	public Some(Dep dep){
    		this.dep = dep
    	}
    	...
    }

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,11 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired() // Use Injection
     public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
-    @GetMapping("/")
+    @GetMapping(path = "/")
     public List<Student> getStudent() {
         return this.studentService.getStudent();
     }
