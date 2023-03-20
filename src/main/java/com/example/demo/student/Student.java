d4d9dbfc7ee03ad3aeb464335084694e8b88ec71
ClydeRM <clyde.official@icloud.com>
2023-03-20 20:09:32 +0800

    feat(Student): Create Student model
    Create Student model
    id: studentId
    name: studentName
    email:
    studentEmail
    age: studentAge
    dataOfBorn: studentDOB

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,5 +1,82 @@
 package com.example.demo.student;
 
-public class Student {
+import java.time.LocalDate;
 
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfBorn;
+
+    // constructor(non)
+    public Student() {
+    }
+    // constructor(*property)
+    public Student(Long id, String name, String email, Integer age, LocalDate dateOfBorn) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBorn = dateOfBorn;
+    }
+
+    // constructor(without id)
+    public Student(String name, String email, Integer age, LocalDate dateOfBorn) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dateOfBorn = dateOfBorn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfBorn() {
+        return dateOfBorn;
+    }
+
+    public void setDateOfBorn(LocalDate dateOfBorn) {
+        this.dateOfBorn = dateOfBorn;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                ", dateOfBorn=" + dateOfBorn +
+                '}';
+    }
 }
