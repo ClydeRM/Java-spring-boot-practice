e74a73628f55115ca57736ce1cb368bd842db4c1
ClydeRM <clyde.official@icloud.com>
2023-03-21 16:54:15 +0800

    feat(Service): Implement UpdateLogic
    Check studentId does exist
    Check body(payload) name property
    format
    Check body(payload) email property format

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,12 +1,12 @@
 package com.example.demo.student;
 
+import com.example.demo.student.DTO.PatchDto;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,10 +36,35 @@ public class StudentService {
     }
 
     public void deleteStudentById(Long studentId) {
-        boolean exists =  studentRepository.existsById(studentId);
+        boolean exists = studentRepository.existsById(studentId);
         if (!exists) {
-            throw new IllegalStateException("StudentId:" + studentId +" does not exists");
+            throw new IllegalStateException("StudentId:" + studentId + " does not exists");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudentById(Long studentId, PatchDto body) {
+        // Check Student exist
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "StudentId" + studentId + " does not exist."
+                ));
+        String name = body.getName();
+        String email = body.getEmail();
+
+        // validate name
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        // check email
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentEmail = studentRepository.findStudentByEmail(email);
+            if (studentEmail.isPresent()){
+                throw new IllegalStateException("Email taken.");
+            }
+            student.setEmail(email);
+        }
     }
 }
