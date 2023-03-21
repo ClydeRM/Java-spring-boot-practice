b50ea93700155e43a479556a7bbcb675103fc918
ClydeRM <clyde.official@icloud.com>
2023-03-21 16:42:01 +0800

    feat(DTO): Create PatchDto
    String name
    String email

---
+++ src/main/java/com/example/demo/student/DTO/patchDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.student.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class patchDto {
+    String name;
+
+    String email;
+}
